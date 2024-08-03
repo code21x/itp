f5b6ccacf06b43e079e3ea4cb6067963128b423c
monish-mukhari <manishmukherjee91@gmail.com>
2024-08-03 16:29:17 +0530

    update signin page

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,29 +1,30 @@
 
 export default function Signin() {
     return <div className="flex justify-center h-screen">
-        <div className="flex flex-col justify-center">
-            <div className="border border-slate-800 text-white px-10 py-6 max-w-lg rounded-md text-3xl">
+        <div className="flex flex-col justify-center pt-32">
+            <div className="border border-slate-800 text-white px-10 pb-12 pt-8 max-w-lg rounded-md text-3xl">
                 <div className="font-semibold text-left pr-24">
                     Signin to your Account
                 </div>
-                <div className="pt-6 pb-4">
-                    <div className="text-lg font-semibold pb-3">
+                <div className="pt-7 pb-4">
+                    <div className="text-lg font-semibold pb-4">
                         Email
                     </div>
                     <div className="">
-                        <input type="email" className="w-full h-10 rounded-md ring-purple-800 ring-2" />
+                        <input type="email" placeholder="name@email.com" className="w-full h-12 rounded-md border border-slate-800 bg-black p-4 pb-4 text-lg focus:border-purple-800 focus:outline-none focus:ring-2 ring-purple-800" />
                     </div>
                 </div>
                 <div className="pb-4">
-                    <div className="text-lg font-semibold pb-3">
+                    <div className="text-lg font-semibold pb-4">
                         Password
                     </div>
                     <div>
-                        <input type="password" className="w-full h-10 rounded-md ring-purple-800" />
+                        <input type="password" placeholder="••••••••" className="w-full h-12 rounded-md bg-black border border-slate-800 p-4 pb-4 text-lg focus:border-purple-800 focus:outline-none focus:ring-2 ring-purple-800" />
+                        
                     </div>
                 </div>
                 
-                <div className="bg-purple-800 text-center py-2 text-black text-lg font-semibold rounded-md">
+                <div className="bg-purple-800 text-center py-3 text-black text-xl font-semibold rounded-md">
                     <button>Login</button>
                 </div>
             </div>
